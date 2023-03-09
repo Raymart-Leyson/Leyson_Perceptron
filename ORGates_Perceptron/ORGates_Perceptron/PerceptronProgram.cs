990204b46ca52d54d6b3f9b78e4da1395fca13c3
Raymart-Leyson <raymart.leyson@cit.edu>
2023-03-09 23:02:39 +0800

    Update PerceptronProgram

--- ORGates_Perceptron/ORGates_Perceptron/PerceptronProgram.cs
+++ ORGates_Perceptron/ORGates_Perceptron/PerceptronProgram.cs
@@ -8,29 +8,48 @@ namespace ORGates_Perceptron
 {
     public class PerceptronProgram
     {
+        public bool running = true;
         public void Run()
         {
             try
             {
-                while (true)
+                while (this.running)
                 {
                     var pt = new PerceptronTraining();
                     var pc = new PerceptronCalculation();
                     Console.WriteLine();
-                    Console.WriteLine("Logical OR Gates Perceptron");
+                    Console.WriteLine("Logical OR Gates Perceptron && Input -1 to End Program");
                     Console.Write("Input Value 1: ");
                     double val1 = Convert.ToDouble(Console.ReadLine());
-                    if (Check(val1) == false) { Error(); break; }
+                    if (Check(val1) == false) { 
+                        if(val1 == -1) running = false;
+                        else
+                            Error(); 
+                        break; 
+                    }
                     Console.Write("Input Value 2: ");
                     double val2 = Convert.ToDouble(Console.ReadLine());
-                    if (Check(val2) == false) { Error(); break; }
+                    if (Check(val2) == false)
+                    {
+                        if (val2 == -1) running = false;
+                        else
+                            Error();
+                        break;
+                    }
                     double[] inputValue = { val1, val2 };
                     double output = pc.Calculate(inputValue, pt.weights, pt.bias);
                     Console.WriteLine("Input Value: [{0}, {1}]\nOutput: {2}\n", inputValue[0], inputValue[1], output);
                     Console.WriteLine();
                     Console.WriteLine("===============================================");
                 }
-            }catch(Exception ex)
+
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Thank You!!!");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            catch(Exception ex)
             {
                 Error();
             }
@@ -46,11 +65,16 @@ namespace ORGates_Perceptron
             Run();
         }
 
+
         public bool Check(double val)
         {
             if (val == 1 || val == 0)
                 return true;
-            return false;
+            else
+            {
+                return false;
+            }
+            
         }
     }
 }
