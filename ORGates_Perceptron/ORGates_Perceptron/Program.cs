d392bbc7bf84a3b47535255d6ce489573869e421
Raymart-Leyson <raymart.leyson@cit.edu>
2023-03-09 22:49:23 +0800

    Update Program

---
+++ ORGates_Perceptron/ORGates_Perceptron/Program.cs
@@ -0,0 +1,11 @@
+﻿using ORGates_Perceptron;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new PerceptronProgram().Run();
+    }
+    
+}
